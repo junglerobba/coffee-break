a2a2fff5fa57597cf64f24e9b71a458d40a2061c
junglerobba <junglerobba@jngl.one>
2026-08-06 15:34:25 +0200

    rust: toolchain and edition upgrade
    latest always

--- src/completions.rs
+++ src/completions.rs
@@ -1,10 +1,10 @@
 use clap::Command;
 use clap_complete::{generate, Generator};
 
-pub(crate) fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
+pub(crate) fn print_completions<G: Generator>(generator: G, cmd: &mut Command) {
     let name = cmd
         .get_bin_name()
         .unwrap_or_else(|| cmd.get_name())
         .to_string();
-    generate(gen, cmd, name, &mut std::io::stdout());
+    generate(generator, cmd, name, &mut std::io::stdout());
 }
